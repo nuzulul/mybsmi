7659bfc706f6740461ae163a31c1340db986bbd2
nuzulul <nuzulz@gmail.com>
2023-12-01 12:40:46 +0700

    test

--- app.js
+++ app.js
@@ -1902,7 +1902,7 @@ function flogger(input)
       
       if (isLocal) return
       if(dashboarddata.user.userbid == 'BSMI3500AA')return
-      if(skipuid.include(dashboarddata.user.useruid)) return
+      if(skipuid.includes(dashboarddata.user.useruid)) return
       
       let login = input
       let nama = dashboarddata.user.usernama
