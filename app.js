14c2202174024f60282e2edd92cf8d253721343f
nuzulul <nuzulul@users.noreply.github.com>
2025-10-13 11:29:09 +0700

    perf : change pin aktivasi to invite code

--- app.js
+++ app.js
@@ -1682,7 +1682,7 @@ $$('.mybsmi-aktivasiscreen').on('click', function () {
 	{
 		aktivasi(pin,true)
 	}else{
-		app.dialog.prompt("PIN", "MyBSMI", async function (pin) {
+		app.dialog.prompt("ENTER INVITE CODE", "MyBSMI", async function (pin) {
 			const buf = await crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(pin));
 			var pinhash = Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
 			//console.log(pinhash);
