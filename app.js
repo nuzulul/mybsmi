ec96fe2e574c3ea3c8d7d27cfbbe9d4357bba4d6
nuzulul <nuzulul@users.noreply.github.com>
2025-11-01 08:36:55 +0700

    fix : fix radio shake multievents output

--- app.js
+++ app.js
@@ -4839,7 +4839,7 @@ function fpagelainnya(){
 				},3000)
 				const code = document.querySelector('#radioterminal').value
 				document.querySelector('#radioterminal').value = '';
-				switch(code){
+				switch(code.toLowerCase()){
 					case "shake":
 						if(shake){
 							shake = false;
@@ -4919,6 +4919,10 @@ function fpagelainnya(){
 				// Lower = more sensitive, higher = less sensitive. 256 works nice, imho.
 				if ((event.rotationRate.alpha > 256 || event.rotationRate.beta > 256 || event.rotationRate.gamma > 256)) {
 					if(shake){
+						shake = false;
+						setTimeout(()=>{
+							shake = true;
+						},3000)
 						radioptt();
 					}
 				}
@@ -4961,7 +4965,7 @@ function fpagelainnya(){
 			pttsoundel.play();			
 		}
         
-        function achex(data){
+        function achex(data,thisws){
           //console.log(data)
           if(data.includes('{"auth":"OK",')) {
             ws.send(`{"joinHub":"${hubname}"}`);
@@ -4980,7 +4984,11 @@ function fpagelainnya(){
             if(json.msg.radioinuse){
 				radiobusy();
             }else if (json.msg.blob){
-				casterid = json.msg.casterid
+				casterid = json.msg.casterid;
+				if(casterid === dashboarddata.user.userbid){
+					thisws.close(true);
+					return;
+				}
 				radiobusy();
 				startsource(json.msg);
             }
@@ -5466,7 +5474,7 @@ function fpagelainnya(){
                 
             ws.onmessage = event => {
                 
-                achex(event.data)            
+                achex(event.data,this)            
                       
             };            
 
@@ -5479,17 +5487,25 @@ function fpagelainnya(){
                 ws.send(JSON.stringify(data))
             };
 
-            ws.onclose = () => {
+            ws.onclose = (dontreconnect) => {
                 console.log('WebSocket connection closed');
-                achexauth = false;
-                let info = document.getElementsByClassName("radioinfo")[0]
-                if(info){
-						info.innerHTML = `
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wifi-off" viewBox="0 0 16 16">
-								  <path d="M10.706 3.294A12.6 12.6 0 0 0 8 3C5.259 3 2.723 3.882.663 5.379a.485.485 0 0 0-.048.736.52.52 0 0 0 .668.05A11.45 11.45 0 0 1 8 4q.946 0 1.852.148zM8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065 8.45 8.45 0 0 1 3.51-1.27zm2.596 1.404.785-.785q.947.362 1.785.907a.482.482 0 0 1 .063.745.525.525 0 0 1-.652.065 8.5 8.5 0 0 0-1.98-.932zM8 10l.933-.933a6.5 6.5 0 0 1 2.013.637c.285.145.326.524.1.75l-.015.015a.53.53 0 0 1-.611.09A5.5 5.5 0 0 0 8 10m4.905-4.905.747-.747q.886.451 1.685 1.03a.485.485 0 0 1 .047.737.52.52 0 0 1-.668.05 11.5 11.5 0 0 0-1.811-1.07M9.02 11.78c.238.14.236.464.04.66l-.707.706a.5.5 0 0 1-.707 0l-.707-.707c-.195-.195-.197-.518.04-.66A2 2 0 0 1 8 11.5c.374 0 .723.102 1.021.28zm4.355-9.905a.53.53 0 0 1 .75.75l-10.75 10.75a.53.53 0 0 1-.75-.75z"/>
-								</svg>						
-						`;
-						setTimeout(()=>{connect()},1000)
+				if(dontreconnect){
+					console.log('Dont reconnect ws');
+				}else{				
+					achexauth = false;
+					let info = document.getElementsByClassName("radioinfo")[0]
+					if(info){
+							info.innerHTML = `
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wifi-off" viewBox="0 0 16 16">
+									  <path d="M10.706 3.294A12.6 12.6 0 0 0 8 3C5.259 3 2.723 3.882.663 5.379a.485.485 0 0 0-.048.736.52.52 0 0 0 .668.05A11.45 11.45 0 0 1 8 4q.946 0 1.852.148zM8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065 8.45 8.45 0 0 1 3.51-1.27zm2.596 1.404.785-.785q.947.362 1.785.907a.482.482 0 0 1 .063.745.525.525 0 0 1-.652.065 8.5 8.5 0 0 0-1.98-.932zM8 10l.933-.933a6.5 6.5 0 0 1 2.013.637c.285.145.326.524.1.75l-.015.015a.53.53 0 0 1-.611.09A5.5 5.5 0 0 0 8 10m4.905-4.905.747-.747q.886.451 1.685 1.03a.485.485 0 0 1 .047.737.52.52 0 0 1-.668.05 11.5 11.5 0 0 0-1.811-1.07M9.02 11.78c.238.14.236.464.04.66l-.707.706a.5.5 0 0 1-.707 0l-.707-.707c-.195-.195-.197-.518.04-.66A2 2 0 0 1 8 11.5c.374 0 .723.102 1.021.28zm4.355-9.905a.53.53 0 0 1 .75.75l-10.75 10.75a.53.53 0 0 1-.75-.75z"/>
+									</svg>						
+							`;
+							setTimeout(()=>{
+
+									connect();
+								
+							},1000)
+					}
 				}
             };
        
